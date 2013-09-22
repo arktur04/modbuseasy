3cf65ac7cd12f96783d58c27f093768c830075c2
arktur04 <arktur04@gmail.com>
2013-09-22 16:08:59 +0600

    Добавлена тестовая программа для modbuseasy.
    Эта программа запускает modbuseasy в режиме slave и по таймеру обновляет значения двух переменных (см. исходник).
    
    Предназначена для тестирования ПО Modbus в режиме Master и для демонстрации работы с modbuseasy как с внешним приложением.

---
+++ Modbus Tester/ModbusTester/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Timers;
+using System.Globalization;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        const int period = 1000;  //timer period in ms
+        const int testVarPeriod = 20;  // test variable period in seconds
+
+        const double fi = Math.PI / 4; //the angle between current and voltage
+        static double alpha;
+
+        static NumberFormatInfo formatProvider;
+
+        static private StreamWriter writer;
+        static private StreamReader reader;
+
+        static void timer_Elapsed(object source, ElapsedEventArgs e)
+        {
+            alpha += 2 * Math.PI / period;
+            writer.WriteLine("setdb CurrentValue {0}", Convert.ToString( Math.Sin(alpha), formatProvider));
+            writer.WriteLine("setdb VoltageValue {0}", Convert.ToString( Math.Sin(alpha + fi), formatProvider));
+        }
+
+        static private System.Timers.Timer timer = new System.Timers.Timer(period);
+        static string mbeDirectory = "C:/Users/V/Desktop/Modbus Tester/ModbusEasy/";
+
+        static string appName = "ModbusEasy.exe";
+        static string appArg = @"config=""" + mbeDirectory + @"config.xml""";
+
+        static void Main(string[] args)
+        {
+
+            formatProvider = new NumberFormatInfo();
+            formatProvider.NumberDecimalSeparator = ".";
+
+            timer.Elapsed += timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Start();
+            //------------------------------------------------------------------
+            //process init
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(mbeDirectory + appName, appArg);
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+
+            Process process = new Process();
+
+            process.StartInfo = processStartInfo;
+            process.Start();
+            writer = process.StandardInput;
+            reader = process.StandardOutput;
+            /*
+            writer.WriteLine("getdb -a");
+            writer.WriteLine("setdb CurrentValue 100.123");
+            writer.WriteLine("getdb CurrentValue");
+             */
+            while (!process.HasExited)
+            {
+                Console.Write((char)reader.Read());
+                if (Console.KeyAvailable && Console.ReadKey().KeyChar == 'q')
+                {
+                    writer.WriteLine("q");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Modbus Tester/ModbusTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ModbusTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModbusTester")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("871bc57a-14ad-4c19-8981-e116784f4213")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
